4560892b3f2182f5a106b26ffbb455c30c2ce8e4
Michael Elliott <elliotecweb@gmail.com>
2014-03-03 01:37:06 -0800

    Update cfruby_public_library.rb

--- cfruby_public_library.rb
+++ cfruby_public_library.rb
@@ -1,13 +1,11 @@
-# Modeling a public library with object oriented Ruby for Code Fellows!
-
 class Library
-	attr_reader :shelf, :books #The library should be aware of a number of shelves.	
+	attr_reader :shelf, :books
 	def initialize
 		@shelf = []
 		@books = []
 	end
 
-	def report_all # The library should have a method to report all books it contains.
+	def report_all
 		puts "Currently in the library we have #{Book.total} books."
 	end
 
@@ -17,7 +15,7 @@ class Library
 end
 
 class Shelf 
-	attr_reader :books #Each shelf should know what books it contains.
+	attr_reader :books
   def initialize(library)
     library.add_shelf(self)
     @books = []
@@ -33,7 +31,7 @@ class Shelf
   end
 end
 
-class Book #Make the book object have "enshelf" and "unshelf" methods that control what shelf the book is sitting on.
+class Book 
 	attr_reader :shelf, :title
 
 		$count = 0
@@ -62,17 +60,17 @@ end
 
 # COMMANDS =====
 
-# Utah = Library.new
-# Fantasy = Shelf.new(Utah)
-# Programming = Shelf.new(Utah)
-# Biography = Shelf.new(Utah)
-# dragons = Book.new("Dragons!", Utah)
-# rubywow = Book.new("Ruby!Wow!", Utah)
-# george = Book.new("The Life of George", Utah)
-# dragons.enshelf(Fantasy)
-# george.enshelf(Biography)
-# rubywow.enshelf(Programming)
-# Utah.report_all
-# dragons.unshelf
-# rubywow.unshelf
-# Utah.report_all
+Utah = Library.new
+Fantasy = Shelf.new(Utah)
+Programming = Shelf.new(Utah)
+Biography = Shelf.new(Utah)
+dragons = Book.new("Dragons!", Utah)
+rubywow = Book.new("Ruby!Wow!", Utah)
+george = Book.new("The Life of George", Utah)
+dragons.enshelf(Fantasy)
+george.enshelf(Biography)
+rubywow.enshelf(Programming)
+Utah.report_all
+dragons.unshelf
+rubywow.unshelf
+Utah.report_all
